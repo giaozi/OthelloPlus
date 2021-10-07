ea807442097be9934bf78c6bc9e2efe9804817da
bw55555 <brianwang2013@gmail.com>
2021-10-07 14:55:04 -0400

    Make websocket static, rename WebsocketConnection to WebsocketManager

--- OthelloPlusUnity/Assets/Scripts/WebsocketManager.cs
+++ OthelloPlusUnity/Assets/Scripts/WebsocketManager.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using WebSocketSharp;
 
-public class WebsocketConnection : MonoBehaviour
+public class WebsocketManager : MonoBehaviour
 {
-    WebSocket ws;
+    public static WebsocketManager Connection = null;
+    private static WebSocket ws;
+
+
     public void Start()
     {
+        if (Connection == null)
+        {
+            Connection = this;
+        }
+        else if (Connection != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Debug.Log("Creating websocket");
         ws = new WebSocket("wss://othellotestserver.bw55555.repl.co");
         ws.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
         ws.OnMessage += (sender, e) =>
@@ -18,28 +31,22 @@ public class WebsocketConnection : MonoBehaviour
         ws.OnError += (sender, e) => {
             Debug.Log($"Websocket Error: {e.Message}");
         };
-
+        ws.OnClose += (sender, e) => {
+            Debug.Log($"Websocket Closed: {e.Reason}");
+        };
         ws.Connect();
     }
     public void Update()
     {
-        if (ws == null)
-        {
-            return;
-        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             WebsocketSendText("Ping");
         }
     }
 
-    private void WebsocketSendText(string text)
+    public static void WebsocketSendText(string text)
     {
-        ws.Send(text);
-    }
-
-    private void WebsocketSendText(string text)
-    {
+        if (ws == null) { Debug.Log("Hi3"); return; }
         ws.Send(text);
     }
 }
